1759f0b306004636f8816226b992088318040ca4
Giuseppe <peppe.mazitto@gmail.com>
2018-12-20 00:05:13 +0100

    Update database.dump.sql

--- php/database.dump.sql
+++ php/database.dump.sql
@@ -1,15 +1,30 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 CREATE TABLE `record` (
   `id` int(11) NOT NULL,
-  `time` text NOT NULL,
-  `location` text NOT NULL,
-  `temperature` text NOT NULL,
-  `humidity` text NOT NULL,
-  `rain` text NOT NULL
+  `data` datetime NOT NULL,
+  `pioggia` int(11) NOT NULL,
+  `temperatura` float NOT NULL,
+  `umidita` float NOT NULL,
+  `pressione` int(11) NOT NULL,
+  `vento` text CHARACTER SET utf8mb4 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `record_live` (
+  `id` int(11) NOT NULL,
+  `data` datetime NOT NULL,
+  `pioggia` int(11) NOT NULL,
+  `temperatura` float NOT NULL,
+  `umidita` float NOT NULL,
+  `pressione` int(11) NOT NULL,
+  `vento` text CHARACTER SET utf8mb4 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 ALTER TABLE `record`
   ADD PRIMARY KEY (`id`);
-
-ALTER TABLE `record`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
-COMMIT;
+  
+ALTER TABLE `record_live`
+  ADD PRIMARY KEY (`id`);
