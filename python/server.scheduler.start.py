41606c907bd7619b5e7d7d7c90f49441233b10d1
Giuseppe <peppe.mazitto@gmail.com>
2018-12-19 21:30:17 +0100

    Delete server.scheduler.start.py

--- python/server.scheduler.start.py
+++
@@ -1,7 +0,0 @@
-from crontab import CronTab
-
-schedule = CronTab(user='pi')
-job = schedule.new(command='python /home/pi/server/server.uploader.py', comment='uploader')
-job.minute.every(1)
-
-schedule.write()
