eb799477a5946f759eee04c788b75cbb0450f4df
Giuseppe <peppe.mazitto@gmail.com>
2018-12-20 00:02:50 +0100

    config.php

---
+++ php/config.php
@@ -0,0 +1,16 @@
+<?php
+	$database = array(
+		'host'=>'sql.hostname',
+		'username'=>'username',
+		'password'=>'password',
+		'database'=>'database_name'
+	);
+	
+	$client = array(
+		'key'=>'your_client_password'
+	);
+	
+	$impostazioni = array(
+		'range'=>'-6'
+	);
+?>
