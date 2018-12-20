9b716f533ea4db04ff805534bb52f47a3776ec51
Giuseppe <peppe.mazitto@gmail.com>
2018-12-20 19:49:35 +0100

    Update meteo_handler.php

--- php/meteo_handler.php
+++ php/meteo_handler.php
@@ -1,13 +1,19 @@
 <?php
+	// importa il file di configurazione
 	include('config.php');
 
+	# inizializza la variabile per la gestione della connessione
 	$conn = mysqli_connect($database['host'], $database['username'], $database['password'], $database['database']);
 	if (mysqli_connect_errno()) {
+		# gestisce l'errore della connessione
 		die("code:error_connect_database");
 	} 
 	
+	// se la chiave di sicurezza del client è uguale a quella del server remoto, allora continua
 	if (htmlspecialchars($_GET['key']) == $client['key']) {
 		switch($_GET['action']) {
+			// Azione: 	requestid
+			// Descrizione:	Ritorna l'ultimo ID cronologicamente
 			case "requestid":
 				$result = mysqli_query($conn, "SELECT id FROM record ORDER BY id DESC LIMIT 1");
 				if ($result) {
@@ -18,6 +24,8 @@
 					echo "code:error_requestid";
 				}
 				break;
+			// Azione:	sync
+			// Descrizione: Esegue la query per la sincronizzazione dei server
 			case "sync":
 				if (mysqli_multi_query($conn, $_GET['query']) === TRUE) {
 					echo "code:success_sync";
@@ -25,6 +33,8 @@
 					echo "code:error_sync";
 				}
 				break;
+			// Azione:	live
+			// Descrizione:	Esegue la query per il salvataggio
 			case "live":
 				if (mysqli_query($conn, $_GET['query']) === TRUE) {
 					echo "code:success_live";
@@ -34,8 +44,10 @@
 				break;
 		}
 	} else {
+		// genera un errore per la chiave di sicurezza
 		echo "code:error_notauthorized";
 	}
 	
+	// chiuse la connessione al database
 	mysqli_close($conn);
 ?>
